618813680444e024b4bfa23cfb5d39715b1793a7
Ahmed Elsayed <138726534+AhmedElsayed30201@users.noreply.github.com>
2023-07-14 16:54:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.imgs');
+let colores = document.querySelector('.container');
+
+
+function iphone(phone){
+          img.src = phone;            
+}
+
+function colors(color){
+          colores.style.background = color;
+}
